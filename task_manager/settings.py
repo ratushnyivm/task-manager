6c7e60a76ad12b14502d0e5c9873fe5676bacb0a
ratushnyyvm <ratushnyyvm@gmail.com>
2023-03-24 16:07:51 +0300

    fix: change the LANGUAGE_CODE to ru

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -137,7 +137,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
